d6ad753f3cc276406c33bf1225b7223453682845
HamidB <ejecteur@yahoo.fr>
2023-06-27 15:48:37 +0200

    modif problem

--- chapter_09/src/main/java/fr/hamtec/locdvd/LocDVDIntentService.java
+++ chapter_09/src/main/java/fr/hamtec/locdvd/LocDVDIntentService.java
@@ -2,6 +2,9 @@ package fr.hamtec.locdvd;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
 import androidx.annotation.Nullable;
 
 public class LocDVDIntentService extends IntentService {
@@ -11,14 +14,26 @@ public class LocDVDIntentService extends IntentService {
     }
     
     @Override
-    protected void onHandleIntent( @Nullable Intent intent ) {
-        
+    protected void onHandleIntent( Intent intent ) {
+        //Récupérer les données étendues de l'intention.
+        //public int getIntExtra (String name, int defaultValue)
+        //1000 la valeur à renvoyer si aucune valeur du type souhaité n'est stockée avec le nom donné.
+        //waitDuration Le nom de l'élément souhaité.
         int waitDuration = intent.getIntExtra( "waitDurarion", 1000 );
+        
+        Messenger messenger = intent.getParcelableExtra( "messager" );
         
         try {
             Thread.sleep( waitDuration );
         }catch ( InterruptedException e ){
             e.printStackTrace();
+        }
+        
+        if ( messenger != null ){
+            Message message = Message.obtain();
+            Bundle replyData = new Bundle();
+            replyData.putString( "reply", "Le service est terminé" );
+            message.setData( replyData );
         }
         
     }
--- chapter_09/src/main/java/fr/hamtec/locdvd/MonActivity.java
+++ chapter_09/src/main/java/fr/hamtec/locdvd/MonActivity.java
@@ -2,9 +2,27 @@ package fr.hamtec.locdvd;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import androidx.annotation.NonNull;
+
+import java.util.logging.Handler;
 
 public class MonActivity extends Activity {
     private  void startService(){
+        
+//        Handler handler = new android.os.Handler(  ){
+//            @Override
+//            public void handleMessage( Message msg ) {
+//                Bundle reply = msg.getData();
+//                String message = reply.getString( "reply" );
+//            }
+//        };
+        
+//        Messenger messenger = new Messenger( ( handler );
+        
         Intent intent = new Intent( MonActivity.this, LocDVDIntentService.class);
         intent.putExtra( "waitDuration", 30_000 );
         startService( intent );
