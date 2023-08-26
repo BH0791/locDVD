917dd56752bcee9d4af6f7eb1b4a4e0d6274bfe2
HamidB <ejecteur@yahoo.fr>
2023-08-26 13:45:18 +0200

    Update -> MainActivity.java SharedPreferences

--- chapter_09/src/main/java/fr/hamtec/locdvd/MainActivity.java
+++ chapter_09/src/main/java/fr/hamtec/locdvd/MainActivity.java
@@ -124,7 +124,7 @@ public class MainActivity extends AppCompatActivity implements ListDVDFragment.O
                         //-ici
                         SharedPreferences sharedPreferences = getSharedPreferences( "fr.hamtec.locDVD.prefs", Context.MODE_PRIVATE );//??
                         SharedPreferences.Editor editor = sharedPreferences.edit( );
-                        editor.putBoolean( "enbeddedDataInsered", true );
+                        editor.putBoolean( "valeurPref", true );
                         editor.apply( );        //- editor.commit()
                         result = true;
                         
@@ -218,16 +218,8 @@ public class MainActivity extends AppCompatActivity implements ListDVDFragment.O
         
         // L'invocation de la méthode readEmbeddeData est conditionnée à l'abscence de la préférence utilisateur
         SharedPreferences sharedPreferences = getSharedPreferences("fr.hamtec.locDVD.prefs", Context.MODE_PRIVATE );
-        
-        
-        if ( !sharedPreferences.getBoolean( "embeddedDataInserted", false ) ){
-            
-            if ( sharedPreferences.contains( "enbeddedDataInsered" ) ){
-                //- TODO
-            }else {
-                readEmbbeddedData();
-            }
-           
+        if ( !sharedPreferences.getBoolean( "valeurPref", false ) ){
+            readEmbbeddedData();
         }
         
     }
